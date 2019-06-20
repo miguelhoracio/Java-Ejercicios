57da00a5ec2f86f401059e49537bb044e706723c
Miguel Horacio Hernandez Balderas <miguel_horaacio@hotmail.com>
2019-06-20 16:31:50 -0500

    2nd activity

--- src/main/java/com/enroutesystems/activities/Act2.java
+++ src/main/java/com/enroutesystems/activities/Act2.java
@@ -1,5 +1,24 @@
 package com.enroutesystems.activities;
 
 public class Act2 {
+	
+	public static double desviacion_estandar(double[] a) {
+		double sum = 0.0;
+		double sum2 = 0.0;
+		double rest = 0.0;
+		double ans = 0.0; 
+		for(int i=0;i<a.length;i++) {
+			sum += a[i];
+		}
+		double prom = sum/a.length;
+		
+		for(int j=0;j<a.length;j++) {
+			rest = a[j]-prom;
+			rest = Math.pow(rest, 2) ;
+			sum2 += rest;
+		}
+		ans = sum2/a.length;
+		return Math.sqrt(ans);		
+	}
 
 }
--- src/main/java/com/enroutesystems/main/App.java
+++ src/main/java/com/enroutesystems/main/App.java
@@ -1,13 +1,24 @@
 package com.enroutesystems.main;
 import com.enroutesystems.activities.Act1;
+import com.enroutesystems.activities.Act2;
 public class App 
 {
 	 public static void main( String[] args )
     {      
+		 
+		 //----------------- Actividad 1------------------- //
+		 
         System.out.println(Act1.producto_mas_caro(Act1.fillProducts()));
         System.out.println(Act1.valor_total_bodega(Act1.fillProducts()));
         System.out.println(Act1.ingreso_total_por_ventas(Act1.fillProducts(),Act1.fillItems()));
         System.out.println(Act1.producto_con_mas_ingresos(Act1.fillProducts(),Act1.fillItems()));
+        
+      //----------------- Actividad 2------------------- //
+        
+        double[] list = {1.3, 1.3, 1.3};
+        System.out.println(Act2.desviacion_estandar(list));
+        
+      //----------------- Actividad 3------------------- //
     }
-    
+     
 }
